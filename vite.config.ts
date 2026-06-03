c41999c50731a0e3d95ae92901f259aaa6ee3618
Qiiii <lqi64949@gmail.com>
2026-06-03 13:45:47 +0800

    fix: update pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/verseform/",
+  base: "/QiDesignKit/",
   plugins: [react()],
   test: {
     environment: "jsdom",
